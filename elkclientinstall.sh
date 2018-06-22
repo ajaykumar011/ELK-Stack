b340f9cdcd8ad912f6f25f29bdb8fe9305fe0031
Ajay Kumar <ajay011.sharma@hotmail.com>
2018-06-22 13:07:07 +0530

    Update elkclientinstall.sh

--- elkclientinstall.sh
+++ elkclientinstall.sh
@@ -10,15 +10,17 @@ rm -rf /etc/yum.repos.d/elastic-beats.repo
 sudo rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch
 echo [beats] >>/etc/yum.repos.d/elastic-beats.repo
 echo name=Elastic Beats Repository >> /etc/yum.repos.d/elastic-beats.repo
-echo baseurl=https://packages.elastic.co/beats/yum/el/$basearch >> /etc/yum.repos.d/elastic-beats.repo
+echo baseurl=https://packages.elastic.co/beats/yum/el/'$basearch' >> /etc/yum.repos.d/elastic-beats.repo
 echo enabled=1 >> /etc/yum.repos.d/elastic-beats.repo
 echo gpgkey=https://packages.elastic.co/GPG-KEY-elasticsearch >> /etc/yum.repos.d/elastic-beats.repo
 echo gpgcheck=1 >> /etc/yum.repos.d/elastic-beats.repo
 
 sudo yum -y install filebeat
-cd ~
 sudo scp elkuser@logio.cloudocean.guru:/etc/pki/tls/certs/logstash-forwarder.crt /tmp
 sudo cp /tmp/logstash-forwarder.crt /etc/pki/tls/certs/
 cp filebeat.yml /etc/filebeat/
 sudo systemctl start filebeat
 sudo systemctl enable filebeat
+echo "------------------------------------------------------------------------"
+echo " Great your client is connected to logio.cloudcean.guru ELK Server"
+echo "------------------------------------------------------------------------"
